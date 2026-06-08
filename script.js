7c6d0637f26aed89f72714e2285418b4069a366e
HaziqNizam <mhaziqknizam@gmail.com>
2026-06-08 11:22:57 +0800

    latest

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-function semakIC() {
-    let ic = document.getElementById("ic").value;
+$(document).ready(function() {
+    $("semak").click(function() 
+    {
+        let ic = $("#ic").val();
 
-    if(ic == "") {
-        alert("Sila masukkan nombor kad pengenalan.");
-    }
-
-    else if (isNaN(ic)) {
-        alert("Kad pengenalan mesti nombor sahaja.");
-    }
-
-    else if (ic.length != 12) {
-        alert("Kad pengenalan mesti 12 digit.");
-    }
-    else {
-        alert("Kad pengenalan sah.");
-    }
-}
+        if (ic == "") {
+            alert("Sila masukkan nombor kad pengenalan.");
+        }
+        else if (isNaN(ic)) {
+            alert("Kad pengenalan mestilah nombor sahaja.");
+        }
+        else if (ic.length != 12) {
+            alert("Kad pengenalan mestilah 12 digit.");
+        }
+        else {
+            alert("Kad pengenalan sah.");
+        }
+    });
+});
